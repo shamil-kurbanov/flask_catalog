82acd569845655e2275520676f67495378d73e76
Shamil Kurbanov <kurbanov@outlook.de>
2024-02-14 15:48:54 +0100

    configured SQLALCHEMY_DATABASE_URI

--- flask_catalog/app.py
+++ flask_catalog/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///temp/test.db'
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
@@ -9,4 +15,4 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
